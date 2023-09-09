4762fadf8731591b09a765f1ba3bf27af6d7b8bd
isadev97 <isa325697@gmail.com>
2023-09-09 20:18:10 +0530

    tags app setup

---
+++ tags/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Tags(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    slug = models.CharField(max_length=200, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        db_table = "tags"
